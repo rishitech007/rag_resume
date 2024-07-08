94000bc45ae5c42e5fb740b0f03aa1c5e2ad18e5
Hrishikesh Rao <rishi.gatech@gmail.com>
2024-07-08 17:28:55 -0400

    Changes to app.py

---
+++ utils/constants.py
@@ -0,0 +1,15 @@
+CHUNK_OVERLAP = 200
+
+CHUNK_SIZE = 1000
+
+EMBEDDING_MODEL = 'text-embedding-3-small'
+
+GPT_MODEL = 'gpt-3.5-turbo'
+
+FAISS_DB = 'faiss-db'
+
+TEMPERATURE = 0.1
+
+PDF_DATA = ['data/resume.pdf']
+
+
